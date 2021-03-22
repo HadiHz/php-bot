886056502566cf909f2f5ebe3ec32bb3a96241c8
Hadi <hadi.haghighizadeh@gmail.com>
2021-03-22 22:06:24 +0430

    telegram api and dotenv package

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$bot_api_key  = $_ENV['BOT_API_KEY'];
+$bot_username = $_ENV['BOT_USERNAME'];
+
+//$mysql_credentials = [
+//    'host'     => 'localhost',
+//    'port'     => 3306, // optional
+//    'user'     => 'dbuser',
+//    'password' => 'dbpass',
+//    'database' => 'dbname',
+//];
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+
+    $telegram->useGetUpdatesWithoutDatabase();
+
+    // Handle telegram getUpdates request
+    $a = $telegram->handleGetUpdates();
+    echo $a;
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // log telegram errors
+     echo $e->getMessage();
+}
 
 echo '<h1>Hello world</h1>';
